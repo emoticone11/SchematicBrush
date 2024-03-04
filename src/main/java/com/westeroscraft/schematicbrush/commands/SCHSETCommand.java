d9d96ceeb7ffa24d9a9c74f1c2f42cbd12ff3ac8
bkane2 <Ben.kane@me.com>
2024-03-04 00:42:29 -0500

    Added command dispatch and stubs for schset

--- src/main/java/com/westeroscraft/schematicbrush/commands/SCHSETCommand.java
+++ src/main/java/com/westeroscraft/schematicbrush/commands/SCHSETCommand.java
@@ -16,7 +16,6 @@ import java.util.regex.Pattern;
 
 import org.enginehub.piston.exception.StopExecutionException;
 
-import com.mojang.brigadier.CommandDispatcher;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.LocalConfiguration;
 import com.sk89q.worldedit.LocalSession;
@@ -43,6 +42,10 @@ import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.regions.RegionSelector;
 import com.sk89q.worldedit.regions.selector.limit.PermissiveSelectorLimits;
 
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.BlockPos;
@@ -60,12 +63,68 @@ public class SCHSETCommand {
 	public static void register(SchematicBrush mod, CommandDispatcher<CommandSourceStack> source) {
 		sb = mod;
 		SchematicBrush.log.info("Register schset");
-		source.register(Commands.literal("schset").executes((ctx) -> {
-			return schSet(ctx.getSource());
-		}));
+    source.register(Commands.literal("schset")
+      .then(Commands.literal("list")
+        .executes(ctx -> schSetList(null, ctx.getSource()))
+        .then(Commands.argument("contains", StringArgumentType.word())
+          .executes(ctx -> schSetList(StringArgumentType.getString(ctx, "contains"), ctx.getSource()))))
+      .then(Commands.literal("create")
+        .then(Commands.argument("setid", StringArgumentType.word())
+          .executes(ctx -> schSetCreate(StringArgumentType.getString(ctx, "setid"), null, ctx.getSource()))
+          .then(Commands.argument("schems", StringArgumentType.greedyString())
+            .executes(ctx -> schSetCreate(StringArgumentType.getString(ctx, "setid"),
+                                          StringArgumentType.getString(ctx, "schems"), ctx.getSource())))))
+      .then(Commands.literal("delete")
+        .then(Commands.argument("setid", StringArgumentType.word())
+          .executes(ctx -> schSetDelete(StringArgumentType.getString(ctx, "setid"), ctx.getSource()))))
+      .then(Commands.literal("append")
+        .then(Commands.argument("setid", StringArgumentType.word())
+          .then(Commands.argument("schems", StringArgumentType.greedyString())
+            .executes(ctx -> schSetAppend(StringArgumentType.getString(ctx, "setid"),
+                                          StringArgumentType.getString(ctx, "schems"), ctx.getSource())))))
+      .then(Commands.literal("remove")
+        .then(Commands.argument("setid", StringArgumentType.word())
+          .then(Commands.argument("schems", StringArgumentType.greedyString())
+            .executes(ctx -> schSetRemove(StringArgumentType.getString(ctx, "setid"),
+                                          StringArgumentType.getString(ctx, "schems"), ctx.getSource())))))
+      .then(Commands.literal("setdesc")
+        .then(Commands.argument("setid", StringArgumentType.word())
+          .then(Commands.argument("desc", StringArgumentType.string())
+            .executes(ctx -> schSetSetDesc(StringArgumentType.getString(ctx, "setid"),
+                                           StringArgumentType.getString(ctx, "desc"), ctx.getSource())))))
+      .then(Commands.literal("get")
+        .then(Commands.argument("setid", StringArgumentType.word())
+          .executes(ctx -> schSetGet(StringArgumentType.getString(ctx, "setid"), ctx.getSource())))));
 	}
 
-	public static int schSet(CommandSourceStack source) {
-		return 1;
-	}
+  public static int schSetList(String contains, CommandSourceStack source) {
+    return 1;
+  }
+
+  public static int schSetCreate(String setid, String schemsStr, CommandSourceStack source) {
+    // TODO: split schemsStr by space
+    return 1;
+  }
+
+  public static int schSetDelete(String setid, CommandSourceStack source) {
+    return 1;
+  }
+
+  public static int schSetAppend(String setid, String schemsStr, CommandSourceStack source) {
+    // TODO: split schemsStr by space
+    return 1;
+  }
+
+  public static int schSetRemove(String setid, String schemsStr, CommandSourceStack source) {
+    // TODO: split schemsStr by space
+    return 1;
+  }
+
+  public static int schSetSetDesc(String setid, String desc, CommandSourceStack source) {
+    return 1;
+  }
+
+  public static int schSetGet(String setid, CommandSourceStack source) {
+    return 1;
+  }
 }
